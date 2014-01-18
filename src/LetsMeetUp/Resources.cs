4e7fba2eb763452501cadb80ea799540685601ee
Tyler Wolf Leonhardt <tyler.wolf.leonhardt@gmail.com>
2014-01-18 12:17:29 -0500

    GUI

--- src/LetsMeetUp/Resources.cs
+++ src/LetsMeetUp/Resources.cs
@@ -11,11 +11,11 @@ namespace LetsMeetUp
     {
         private readonly List<DummyUser> _dummyUsers = new List<DummyUser>
         {
-            new DummyUser("Tyler", 21, "Brace for impact."),
-            new DummyUser("Jon", 21, "Hello."),
-            new DummyUser("Liz", 21, "I like men."),
-            new DummyUser("Chirag", 22, "I like food."),
-            new DummyUser("Lisa", 19, "I like my computer.")
+            new DummyUser("Tyler", 21, "Brace for impact.", "../Assets/Tyler.jpg"),
+            new DummyUser("Jon", 21, "Hello.", "../Assets/Jon.jpg"),
+            new DummyUser("Liz", 21, "I like men.", "../Assets/Lizzy.jpg"),
+            new DummyUser("Chirag", 22, "I like food.", "../Assets/Chirag.jpg"),
+            new DummyUser("Lisa", 19, "I like my computer.", "../Assets/Lisa.jpg")
         };
         
         public List<DummyUser> DummyUsers { get { return _dummyUsers; } }
--- src/LetsMeetUp/ViewModel/DummyUser.cs
+++ src/LetsMeetUp/ViewModel/DummyUser.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Windows.Controls;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,13 +21,17 @@ namespace LetsMeetUp.ViewModel
         private string _tagLine;
         public string TagLine { get { return _tagLine; } set { _tagLine = value; if (PropertyChanged != null) PropertyChanged(this, new PropertyChangedEventArgs("TagLine")); } }
 
+        private string _imagePath;
+        public string ImagePath { get { return _imagePath; } set { _imagePath = value; if (PropertyChanged != null) PropertyChanged(this, new PropertyChangedEventArgs("ImagePath")); } }
+
         public event PropertyChangedEventHandler PropertyChanged;
 
-        public DummyUser(string firstName, int age, string tagLine)
+        public DummyUser(string firstName, int age, string tagLine, string imagePath)
         {
             FirstName = firstName;
             Age = age;
             TagLine = tagLine;
+            ImagePath = imagePath;
         }
     }
 }
